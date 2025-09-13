69bf89c7f559e4bbe7d61fd8ba3eea5f72cbebff
André Siqueira <andre@siqueira.dev>
2025-09-13 07:05:53 -0300

    Adicionar gerenciamento de IDs e novos métodos na API de filmes
    Implementada a atribuição de IDs únicos aos filmes na classe `FilmeController`. O método `AdicionarFilme` agora retorna um `IActionResult`, permitindo respostas HTTP adequadas. Adicionados métodos para recuperar filmes com paginação e buscar filmes por ID. Também foi incluída a propriedade `Id` na classe `Filme` para identificar cada filme de forma única.

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -8,12 +8,27 @@ namespace FilmesApi.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
-    public void AdicionarFilme([FromBody] Filme filme)
+    public IActionResult AdicionarFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine($"Filme '{filme.Titulo}' adicionado com sucesso!");
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
     }
 
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery]int skip = 0, [FromQuery]int take = 50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok();
+    }
 }
